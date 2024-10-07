468de7fa69698ddf2aec92604808083360a8788d
Bruno Pavan Adario Moreira <bpmoreira@TOPAZ.CORP>
2024-10-07 17:20:10 -0300

    feat(web-api): initial commit

---
+++ src/main/java/br/com/bruno/my_first_web_api/controller/UserController.java
@@ -0,0 +1,36 @@
+package br.com.bruno.my_first_web_api.controller;
+
+import br.com.bruno.my_first_web_api.model.User;
+import br.com.bruno.my_first_web_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{username}")
+    public User getByUsername(@PathVariable("username") String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping
+    public void saveUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
+}
---
+++ src/main/java/br/com/bruno/my_first_web_api/model/User.java
@@ -0,0 +1,14 @@
+package br.com.bruno.my_first_web_api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String login;
+    private String password;
+}
